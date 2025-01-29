47afa3b8b7d1a8940bbbda91362f662fb10ad77a
Pazos-956 <abelpazb@gmail.com>
2025-01-29 13:33:55 +0100

    Archivo de instalación

---
+++ install.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+stow_folders="bspwm kitty networkmanager-dmenu
+nvim picom polybar rofi scripts session sxhkd zsh" #eww
+
+for folder in $(echo $stow_folders)
+do
+    stow -D $folder
+    stow $folder
+done
+
+apps="lsd bat neovim kitty
+rofi picom polybar bspwm sxhkd 
+zsh zsh-syntax-highlighting zsh-autosuggestions zsh-theme-powerlevel10k-git
+flameshot firefox-developer-edition pcmanfm"
+
+yay -S $apps
