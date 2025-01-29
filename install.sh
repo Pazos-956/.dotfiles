b30b92d64510beef1e17bf2de24d22f38de0b4e3
Pazos-956 <abelpazb@gmail.com>
2025-01-29 13:38:02 +0100

    añadido feh y udiskie

--- install.sh
+++ install.sh
@@ -11,6 +11,8 @@ done
 apps="lsd bat neovim kitty
 rofi picom polybar bspwm sxhkd 
 zsh zsh-syntax-highlighting zsh-autosuggestions zsh-theme-powerlevel10k-git
-flameshot firefox-developer-edition pcmanfm"
+flameshot firefox-developer-edition pcmanfm udiskie feh"
 
 yay -S $apps
+
+feh --bg-fill $HOME/Pictures/fondos/piano_astronaut.png &
