e1772d99c9953ac10e7aa7297160afaed94d0a20
Pazos-956 <abelpazb@gmail.com>
2025-02-01 13:25:26 +0100

    añadido xclip para que vaya unnamedplus en nvim

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 apps="lsd bat neovim kitty
-rofi picom polybar bspwm sxhkd stow
+rofi picom polybar bspwm sxhkd stow xclip
 zsh zsh-syntax-highlighting zsh-autosuggestions zsh-theme-powerlevel10k-git
 flameshot firefox-developer-edition pcmanfm udiskie feh"
 
